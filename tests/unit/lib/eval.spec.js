d0c5b5945db0b307839f65651d2750d402570f4c
Mike Douglas <michael.j.douglas@gmail.com>
2020-03-14 08:54:08 -0400

    Added to test cases

--- tests/unit/lib/eval.spec.js
+++ tests/unit/lib/eval.spec.js
@@ -2,10 +2,26 @@
 import evaluateString from '@/lib/eval.js';
 
 describe('evaluateString', () => {
-  it('parses a string into a JSON object', () => {
+  it('evaluates a single variable', () => {
     const text = 'DBMarket';
     expect(evaluateString(text, {
       DBMarket: 100,
     })).toEqual(100);
+  });
+
+  it('evaluates the min function between two variables', () => {
+    const text = 'min(DBMarket, DBMinBuyout)';
+    expect(evaluateString(text, {
+      DBMarket: 100,
+      DBMinBuyout: 20,
+    })).toEqual(20);
+  });
+
+  it('evaluates the addition of two variables', () => {
+    const text = 'DBMarket + DBMinBuyout';
+    expect(evaluateString(text, {
+      DBMarket: 100,
+      DBMinBuyout: 20,
+    })).toEqual(120);
   });
 });
